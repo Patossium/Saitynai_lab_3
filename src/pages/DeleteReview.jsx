fcc2d3ec31dcb7f4b2192568f806f91334a86eb6
Patossium <bliuv.simona@gmail.com>
2022-12-18 19:20:20 +0200

    Added review detele functionality

--- src/pages/DeleteReview.jsx
+++ src/pages/DeleteReview.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import { useContext } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+import MyContext from '../context/MyContext';
 
 const DeleteReview = () => {
-	return <main>DeleteReview</main>;
+	const { state } = useLocation();
+	const { review } = state;
+	const { id, text, rating, book: { id: bookId, name, author, rating: bookRating, genre }} = review;
+
+	const navigate = useNavigate();
+
+	const {token} = useContext(MyContext);
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+
+		//Post form to backend API
+		try {
+			const config = {
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + token,
+				},
+			};
+
+			const response = await axios.delete(
+				`https://jellyfish-app-ynzfh.ondigitalocean.app/api/books/${bookId}/reviews/${id}`,
+				config,
+			);
+
+			navigate('/');
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	return (
+		<main>
+			<section className="form">
+				<form>
+					<p>{text}</p>
+					<p>{rating}</p>
+
+					<button onClick={submitHandler}>Delete Review</button>
+				</form>
+			</section>
+		</main>
+	);
 };
 
 export default DeleteReview;
